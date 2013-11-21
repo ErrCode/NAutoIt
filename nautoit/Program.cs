b8488d0d77c17fb095cdbe3d3c8d46a572c9a176
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-11-21 18:48:36 +0300

    clean up of C#

--- nautoit/Program.cs
+++ nautoit/Program.cs
@@ -4,50 +4,35 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using AutoItX3Lib;
 using NRegFreeCom;
 using NRegFreeCom.Interop.ComTypes;
+using TestDispatchUtility;
 
 namespace nautoit
 {
+  
 
     class Program
     {
-
+   
         /// testing calls in C# before python
         static void Main(string[] args)
         {
+            var clsid = new Guid("{1A671297-FA74-4422-80FA-6C5D8CE4DE04}");
 
-            var createdViaManifest = NRegFreeCom.ActivationContext.CreateInstanceWithManifest(new Guid("{1A671297-FA74-4422-80FA-6C5D8CE4DE04}"), "AutoItX3Dependency.manifest");
+            var createdViaManifest = NRegFreeCom.ActivationContext.CreateInstanceWithManifest(clsid, "AutoItX3Dependency.manifest");
             var autoItWithManifest = (IAutoItX3)createdViaManifest;
             autoItWithManifest.Run("Notepad");
 
-            var clsid = new Guid("{1A671297-FA74-4422-80FA-6C5D8CE4DE04}");
+           
             var assemblies = new NRegFreeCom.AssemblySystem();
             var module = assemblies.LoadFrom(Path.Combine(Environment.CurrentDirectory, "AutoItX3.dll"));
-            var rg = NRegFreeCom.ActivationContext.CreateInstanceDirectly(module, clsid);
-            var run = (IAutoItX3)rg;
-            try
-            {
-                var t = Type.GetType("System.OleAutBinder");
-                var binder = System.Activator.CreateInstance(t);
-                run.GetType().InvokeMember("Run", BindingFlags.Instance | BindingFlags.Public | BindingFlags.InvokeMethod, (Binder)binder, run, new object[] { "Notepad", "", 1 });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
+            var createdDirectly = NRegFreeCom.ActivationContext.CreateInstanceDirectly(module, clsid) as IAutoItX3;
+            createdDirectly.Run("Notepad");
 
-          
-            try
-            {
-                run.Run("Notepad");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
         }
     }
 }
