78c41696a6b4d142803a22faec762fbb5b5785ce
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-11-21 19:36:47 +0300

    clean up of C#

--- nautoit/Program.cs
+++ nautoit/Program.cs
@@ -1,24 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
 using AutoItX3Lib;
-using NRegFreeCom;
-using NRegFreeCom.Interop.ComTypes;
-using TestDispatchUtility;
 
 namespace nautoit
 {
-  
-
     class Program
     {
-   
-        /// testing calls in C# before python
+        /// testing calls in C# before IronPython
         static void Main(string[] args)
         {
             var clsid = new Guid("{1A671297-FA74-4422-80FA-6C5D8CE4DE04}");
@@ -27,7 +15,7 @@ namespace nautoit
             var autoItWithManifest = (IAutoItX3)createdViaManifest;
             autoItWithManifest.Run("Notepad");
 
-           
+
             var assemblies = new NRegFreeCom.AssemblySystem();
             var module = assemblies.LoadFrom(Path.Combine(Environment.CurrentDirectory, "AutoItX3.dll"));
             var createdDirectly = NRegFreeCom.ActivationContext.CreateInstanceDirectly(module, clsid) as IAutoItX3;
