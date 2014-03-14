b2cbd73820b431cf266712b27296592a5410adb0
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2014-03-14 18:24:21 +0300

    added white sample

--- white_integration.py
+++ white_integration.py
@@ -5,17 +5,13 @@ import sys
 import os
 import clr
 
-path = os.path.dirname(__file__)
-sys.path.append(os.getcwd()+"/packages/Castle.Core.3.2.2/lib/net35")
-sys.path.append(os.getcwd()+"/packages/TestStack.White.0.12.0/lib/net40")
+clr.AddReferenceToFile("nautoit.dll")   
 
-clr.AddReferenceToFile("nautoit.dll")     
 clr.AddReferenceToFile("TestStack.White.dll")     
 
 from nautoit import au
 from  TestStack.White import Application
 from  TestStack.White.InputDevices import Keyboard
-
 
 application = Application.Launch("notepad.exe");
 window = application.GetWindow("Untitled - Notepad");
