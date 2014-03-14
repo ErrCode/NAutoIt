90dff57ff96586a745d7ba12b0c2de4bc28e11b5
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2014-03-14 18:20:43 +0300

    added autoxpackage

--- white_integration.py
+++ white_integration.py
@@ -1,25 +1,27 @@
 ﻿
-#IronPython case use other .NET testing framework which depend on MS UI Automation, can call Automation directly
+#IronPython case use other .NET testing framework like MS UI Automation or White
 
 import sys
 import os
-path = os.path.dirname(__file__)
+import clr
 
+path = os.path.dirname(__file__)
 sys.path.append(os.getcwd()+"/packages/Castle.Core.3.2.2/lib/net35")
 sys.path.append(os.getcwd()+"/packages/TestStack.White.0.12.0/lib/net40")
-import clr
-clr.AddReference("System")
-clr.AddReference("System.Core")
-clr.AddReference("System.Windows.Forms")
-clr.AddReferenceToFile("Castle.Core.dll")
+
+clr.AddReferenceToFile("nautoit.dll")     
 clr.AddReferenceToFile("TestStack.White.dll")     
 
+from nautoit import au
 from  TestStack.White import Application
 from  TestStack.White.InputDevices import Keyboard
+
 
 application = Application.Launch("notepad.exe");
 window = application.GetWindow("Untitled - Notepad");
 window.Focus()
-Keyboard.Instance.HoldKey(White.Core.WindowsAPI.KeyboardInput.SpecialKeys.CONTROL);
-Keyboard.Instance.Enter("E");
+Keyboard.Instance.Enter("This is some text.");
 window.Close()
+#Allows step by step migration from AutoIt to anything else
+close = au.WinWaitActive("Notepad", "Save")
+au.Send("!n")
